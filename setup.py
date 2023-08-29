1a6b18124e7d4996218d66eefc4590b60a4f9850
luojie <1342104001@qq.com>
2023-08-29 16:30:01 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
+def version():
+    from oauth2tools import __version__
+    return __version__
+
+
+setup(
+    name="oauth2tools",
+    version=version(),
+    author="Bean-jun",
+    author_email="1342104001@qq.com",
+    description="oauth2 tools",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=[
+        "Flask>=2.3.2",
+        "requests>=2.31.0",
+    ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
